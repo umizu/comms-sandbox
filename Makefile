59f1b062d2993981adc5d0586e8320c8404aa076
Marc <ninjaz.gg@outlook.com>
2024-03-31 14:53:14 -0400

    long-polling example

--- Makefile
+++ Makefile
@@ -5,5 +5,9 @@ push:
 short-poll:
 	@go build -o ./bin/short-poll ./short-poll
 	@./bin/short-poll
+
+long-poll:
+	@go build -o ./bin/long-poll ./long-poll
+	@./bin/long-poll
 	
-.PHONY: push short-poll
+.PHONY: push short-poll long-poll
---
+++ long-poll/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/random"
+)
+
+var jobs = make(map[string]*Job)
+
+func main() {
+	server := NewAPIServer(":8080")
+	server.Start()
+}
+
+func (s *APIServer) Start() {
+	s.router.POST("/submit", submitHandler)
+	s.router.GET("/checkstatus", checkStatusHandler)
+
+	s.router.Logger.Fatal(s.router.Start(s.listenAddr))
+}
+
+type APIServer struct {
+	router     *echo.Echo
+	listenAddr string
+}
+
+type Job struct {
+	Progress int
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		router:     echo.New(),
+		listenAddr: listenAddr,
+	}
+}
+
+func submitHandler(c echo.Context) error {
+	id := random.String(7) // weak
+	jobs[id] = &Job{
+		Progress: 0,
+	}
+	go updateJob(id)
+	return c.String(http.StatusOK, fmt.Sprintf("job:%s", id))
+}
+
+func checkStatusHandler(c echo.Context) error {
+	id := c.QueryParam("jobId")
+	job := jobs[id]
+	if job == nil {
+		return c.String(http.StatusNotFound, fmt.Sprintf("jobId %s not found", id))
+	}
+
+	wg := &sync.WaitGroup{} // seeing how waitgroups work, kinda unnecessary here
+	wg.Add(2)
+
+	go func(job *Job, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for job.Progress < 100 {
+			time.Sleep(3 * time.Second)
+		}
+	}(job, wg)
+
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		// do things
+	}(wg)
+
+	wg.Wait()
+	return c.String(http.StatusOK, fmt.Sprintf("status:%d%%", job.Progress))
+}
+
+func updateJob(id string) {
+	for jobs[id].Progress < 100 {
+		time.Sleep(5 * time.Second)
+		jobs[id].Progress += 10
+	}
+}
